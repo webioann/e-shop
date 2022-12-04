37b5e085bddfbc511ca7407e809b498915265176
Webioann <ioannn7777@gmail.com>
2022-12-04 20:01:07 +0200

    project is work git add .git add .!

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+const  App: React.FC = () => {
+  
+  return (
+    <h1>E-COMMERCE</h1>
+  )
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
+// import store from "./Redux/store"
+// import { Provider } from 'react-redux'
+// import { BrowserRouter } from 'react-router-dom'
+// import './Firebase/firebase-config'
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+    <App />
+);
