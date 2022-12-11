759a1edbc7c636bd30457ba4381dcbc77e4e2a4a
Webioann <ioannn7777@gmail.com>
2022-12-11 08:28:46 +0200

    delete consol.log in auth hook

--- src/hooks/useAuthStateCurrentUser.ts
+++ src/hooks/useAuthStateCurrentUser.ts
@@ -25,7 +25,6 @@ export const useAuthStateCurrentUser = () => {
                     userID: user.uid,
                     userPhotoURL: user.photoURL,
                 }))
-                console.log(user)
             }
             else{
                 dispatch(deleteCurrentUser())
